63a6b22a83df06cc49fe36b5a4cccfae8b0c680c
Mehedi Hasan <mehedihtanvir@gmail.com>
2024-09-05 21:39:00 +0600

    fixed clashes of project showcase grids

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -7,15 +7,15 @@ import { FaLocationArrow } from "react-icons/fa6";
 const RecentProjects = () => {
   return (
     <div className="py-20" id="projects">
-      <h1 className="heading">
+      <h1 className="heading mt-10">
         A small selection of my{" "}
         <span className="text-purple">recent projects</span>
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-2 p-4">
+      <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-3 gap-x-8 gap-y-2 p-4">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="flex items-center justify-center sm:h-[41rem] h-[32rem]"
+            className="flex items-center justify-center mt-10 h-[25rem] sm:h-[25rem] md:h-[35rem] lg:h-[35rem] xl:h-[35rem] 2xl:h-[35rem]"
           >
             <PinContainer href={link} title={title}>
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh]">
@@ -31,7 +31,7 @@ const RecentProjects = () => {
                   className="z-10 abosolute bottom-0"
                 />
               </div>
-              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
+              <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1 mt-4 mb-2">
                 {title}
               </h1>
               <p
